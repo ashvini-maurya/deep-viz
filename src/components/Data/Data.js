086f24cf04ee9e66b92ad79c4e886a10ca3413c6
ashvinikumar <ashvinikumar45@gmail.com>
2020-10-12 10:23:02 +0530

    pagination added

--- src/components/Data/Data.js
+++ src/components/Data/Data.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import ReactPaginate from "react-paginate";
 import ChartWithTimestamp from "../ChartWithTimestamp/ChartWithTimestamp";
 import ChartWithoutTimestamp from "../ChartWithoutTimestamp/ChartWithoutTimestamp";
 
 const Data = () => {
   const [cvedata, setCVEdata] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [sliceData, setSliceData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -12,25 +15,42 @@ const Data = () => {
         "http://52.152.146.89:8007/cve-data.json"
       );
       setCVEdata(request.data);
+      setPageCount(Math.ceil(request.data.length / 100));
+      setSliceData(request.data.slice(0, 100));
       return request;
     };
     fetchData();
   }, []);
 
-  console.log("hello: ", cvedata);
+  const handlePageClick = (e) => {
+    setSliceData(cvedata.slice(e * 100 - 100, e * 100));
+  };
 
   return (
-    <div>
+    <>
       <h2>Deepfence</h2>
-      {cvedata?.length > 0 ? (
+      {sliceData?.length > 0 ? (
         <>
-          <ChartWithTimestamp cvedata={cvedata} />
-          <ChartWithoutTimestamp cvedata={cvedata} />
+          <ReactPaginate
+            previousLabel={"previous"}
+            nextLabel={"next"}
+            breakLabel={"..."}
+            breakClassName={"break-me"}
+            pageCount={pageCount}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            onPageChange={(e) => handlePageClick(e.selected + 1)}
+            containerClassName={"pagination"}
+            subContainerClassName={"pages pagination"}
+            activeClassName={"active"}
+          />
+          <ChartWithTimestamp cvedata={sliceData} />
+          <ChartWithoutTimestamp cvedata={sliceData} />
         </>
       ) : (
         <p>Loading...!</p>
       )}
-    </div>
+    </>
   );
 };
 
